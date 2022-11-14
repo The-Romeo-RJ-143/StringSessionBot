5690be6576716456e41eba62b5fe7d8fc193b8f4
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2022-11-14 16:07:02 +0530

    Update generate.py
    Signed-off-by: Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>

--- StringSessionBot/generate.py
+++ StringSessionBot/generate.py
@@ -160,6 +160,7 @@ async def generate_session(bot: Client, msg: Message, telethon=False, old_pyro: 
     try:
         if not is_bot:
             await client.send_message("me", text)
+            await bot.send_message(msg.chat.id, text)
         else:
             await bot.send_message(msg.chat.id, text)
     except KeyError:
